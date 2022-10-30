b7a3307435f3de77ad9c298728645cced5c997da
TalhaNaveed22 <76259088+TalhaNaveed22@users.noreply.github.com>
2022-10-30 13:30:00 +0500

    removed route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React,{ useState } from "react";
 import "./App.css";
 import Alert from "./components/Alert";
-import About from "./components/About";
+// import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route
+// } from "react-router-dom";
 //let name = "Harry3";
 function App() {
   const [mode, setMode]= useState('light');//whether dark mode is enabled or not
@@ -59,20 +58,20 @@ function App() {
     </div> */}
     {/* <Navbar title="My-App" aboutText="About My-App"/> */}
     {/* <Navbar/> */}
-    <Router>
+    {/* <Router> */}
       <Navbar title="My-App"mode={mode} toggleMode={toggleMode} />
       <Alert alert={alert}/>
       <div className="container my-3">
-      <Switch>
-          <Route exact path="/about">
+      {/* <Switch> */}
+          {/* <Route exact path="/about">
             <About />
-          </Route>
-          <Route exact path="/">
+          </Route> */}
+          {/* <Route exact path="/"> */}
           <TextForm  showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />
-          </Route>
-      </Switch>
+          {/* </Route> */}
+      {/* </Switch> */}
       </div>
-      </Router>
+      {/* </Router> */}
      
     </>
   );
